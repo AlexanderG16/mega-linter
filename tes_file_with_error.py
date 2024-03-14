29e1c117b0b052f53877ada058a58f1ed066fc2b
Mikael Alexander Gunawan <alexandergun16@gmail.com>
2024-03-14 21:52:07 +0700

    Add File with Error to check

---
+++ tes_file_with_error.py
@@ -0,0 +1,7 @@
+def add_numbers(a, b):
+    # Missing colon after the function definition
+    return a + b
+
+result = add_numbers(3, 4)
+# Variable name 'Result' instead of 'result'
+Print("The result is: ", Result)
